1cbe49ffd7da11ec1ed1d04f0ed9f3a69937ab13
Alexis La Porte <aklaporte@gmail.com>
2022-12-15 02:39:53 -0500

    Fix whitespace

--- day-1/ruby/solution.rb
+++ day-1/ruby/solution.rb
@@ -1,5 +1,6 @@
 lines = File.open('../inputs/test.txt').read.split("\n\n")
-calories_per_elf = lines.map { |l| l.split("\n").map(&:to_i).sum}.sort
+calories_per_elf = lines.map { |l| l.split("\n").map(&:to_i).sum }.sort
+
 puts "The highest calorie load was #{calories_per_elf.last}."
 puts "The top 3 highest calorie loads were: %d, %d, and %d." % calories_per_elf.last(3)
 
