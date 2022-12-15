713a5643bf84030facf56d08b4cb6894035e8f82
Alexis La Porte <aklaporte@gmail.com>
2022-12-15 12:53:39 -0500

    Refactor, clean up formatting

--- day-4/ruby/solution.rb
+++ day-4/ruby/solution.rb
@@ -2,24 +2,23 @@ require 'set'
 
 # Process the entire txt file to get count
 def count_overlap(lines, option={})
-    contain_count = lines.reduce(0) do |count, line|
+    lines.reduce(0) do |count, line|
         id_pair = sanitize(line)
         a = id_pair.first
         b = id_pair.last
         count += ((is_overlapping(a, b, option)) ? 1 : 0)
     end
-    contain_count
 end
 
-# Takes a line and removes punctuation. Returns an array of 2 arrays with 2 ints in the inner arrays representing ranges, for ex: [[1, 2], [3, 4]]
+# Takes a line and removes punctuation. Returns an array of 2 arrays with 2 \ ints in the inner arrays representing ranges, for ex: [[1, 2], [3, 4]]
 def sanitize(str)
     return str.strip()
-    .gsub('-', ' ')
-    .split(',')
-    .map { |line| line.split(' ').map(&:to_i) }
+                .gsub('-', ' ')
+                .split(',')
+                .map { |line| line.split(' ').map(&:to_i) }
 end
 
-# Takes two arrays of ints and an option about which condition to check for, returns boolean value
+# Takes two arrays of ints and an option about which condition to check for, \ returns boolean value
 def is_overlapping(a, b, options={})
     a_range = (a.first..a.last)
     b_range = (b.first..b.last)
