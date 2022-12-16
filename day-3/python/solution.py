20c40147304a04cf541436256e2d3ecdd3a46b01
Alexis La Porte <aklaporte@gmail.com>
2022-12-16 02:42:31 -0500

    Make method accept different sizes

--- day-3/python/solution.py
+++ day-3/python/solution.py
@@ -18,11 +18,14 @@ def get_sum(rucksacks):
     return sum
 
 
-def get_group_sum(badges, size):
+def get_group_sum(rucksacks, size):
     sum = 0
-    for i in range(0, len(badges), size):
-        badge = set(badges[i]).intersection(set(badges[i+1])).intersection(set(badges[i+2])).pop()
-        sum += get_priority_score(badge)
+    for i in range(0, len(rucksacks), size):
+        group_rucksacks = [set(rucksacks[i]) for i in range(i, i+size)]
+        badge_set = group_rucksacks[0]
+        for item_set in group_rucksacks[1:]:
+            badge_set = badge_set.intersection(item_set)
+        sum += get_priority_score(badge_set.pop())
     return sum
 
 
@@ -30,7 +33,7 @@ def display(sum):
     print("Priorities sum is {:,}.".format(sum))
 
 
-with open('../inputs/input.txt', 'r') as f:
+with open('../inputs/test.txt', 'r') as f:
     lines = f.read().splitlines()
     display(get_sum(lines))
     display(get_group_sum(lines, 3))
