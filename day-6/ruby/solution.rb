34cb917d86fecd4436c10b35324f7dac95147a3e
Alexis La Porte <aklaporte@gmail.com>
2022-12-17 23:00:21 -0500

    Rename variables and re-indent

--- day-6/ruby/solution.rb
+++ day-6/ruby/solution.rb
@@ -2,10 +2,11 @@ lines = File.open('../inputs/input.txt').readlines.map(&:chomp)
 
 def get_marker_index(lines, packet_size)
     lines.map do |line|
-        match = line.chars
-                    .each_cons(packet_size)
-                    .with_index.find {|sub, i| sub.uniq.size == packet_size }
-        match.last + packet_size
+        _, match_idx = line.chars
+                            .each_cons(packet_size)
+                            .with_index
+                            .find {|substr, i| substr.uniq.size == packet_size }
+        match_idx + packet_size
     end
 end
 
