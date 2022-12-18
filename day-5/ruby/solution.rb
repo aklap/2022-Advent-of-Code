7e674fa60c3ce24d04f6aac0f19d636486b34385
Alexis La Porte <aklaporte@gmail.com>
2022-12-17 19:56:26 -0500

    Remove comments

--- day-5/ruby/solution.rb
+++ day-5/ruby/solution.rb
@@ -1,20 +1,18 @@
 lines  = File.read('../inputs/test.txt').split("\n\n")
-# Create instructions
 def get_instructions(lines)
     instructions = lines[1].split("\n").map { |instruct| instruct.scan(/\d+/).map(&:to_i) }
 end
-# Create rows
+
 def get_rows(lines)
     rows = lines[0].split("\n")[0..-2].map{ |row| row.chars }
-        # gsub(/[\[\]]/, ' ') }
 end
-# Create stacks, transpose rows
+
 def transpose(rows)
     rows.transpose.reduce([]) do |cols, col| 
         col.grep(/[A-Z]/).any? ?  cols << col.grep(/[A-Z]/) : cols
     end
 end
-# move one crate
+
 def move_one(stacks, n, move_start, move_end)
     n.times do
         removed = stacks[move_start].shift
@@ -22,13 +20,13 @@ def move_one(stacks, n, move_start, move_end)
     end
     stacks
 end
-# move multiple crates
+
 def move_multiple(stacks, n, move_start, move_end)
     removed = stacks[move_start].slice!(0, n)
     stacks[move_end].unshift(*removed)
     stacks
 end
-# move crates
+
 def move_crates(stacks, instructions, options={})
     instructions.each do |instruction|
         n_crates = instruction[0]
@@ -43,14 +41,13 @@ def move_crates(stacks, instructions, options={})
     end
     stacks
 end
-# Prints message from moved crates
+
 def display_message(stacks)
     puts "Message is #{ stacks.map(&:first).join('') }"
 end
 
 rows = get_rows(lines)
 stacks = transpose(rows) 
-p stacks
 instructions = get_instructions(lines)
 # test one
 moved = move_crates(stacks, instructions, one: true)
