3d09ce6160e4fd778b0e28d4a7eaf2a36b238af4
Alexis La Porte <aklaporte@gmail.com>
2022-12-17 23:06:27 -0500

    Pretty print the return values

--- day-6/ruby/solution.rb
+++ day-6/ruby/solution.rb
@@ -1,14 +1,14 @@
-lines = File.open('../inputs/input.txt').readlines.map(&:chomp)
+lines = File.open('../inputs/test.txt').readlines.map(&:chomp)
 
 def get_marker_index(lines, packet_size)
     lines.map do |line|
         _, match_idx = line.chars
                             .each_cons(packet_size)
                             .with_index
-                            .find {|substr, i| substr.uniq.size == packet_size }
+                            .find { |str, i| str.uniq.size == packet_size }
         match_idx + packet_size
     end
 end
 
-p get_marker_index(lines, 4)
-p get_marker_index(lines, 14)
+puts "Markers ending at the following indices for packet size 4: #{get_marker_index(lines, 4).join(', ')}"
+puts "Markers ending at the following indices for packet size 14: #{get_marker_index(lines, 14).join(', ')}"
