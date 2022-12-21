64295b98c15f3c6de702f3f8582dbc5af4fa3486
Alexis La Porte <aklaporte@gmail.com>
2022-12-21 13:19:54 -0500

    Remove whitespace

--- day-1/ruby/solution.rb
+++ day-1/ruby/solution.rb
@@ -3,5 +3,3 @@ calories_per_elf = lines.map { |l| l.split("\n").map(&:to_i).sum }.sort
 
 puts "The highest calorie load was #{calories_per_elf.last}."
 puts "The top 3 highest calorie loads were: %d, %d, and %d." % calories_per_elf.last(3)
-
-
