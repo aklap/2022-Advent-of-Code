95d6b72b760e619c3a8db12869c96c291de1d72a
Alexis La Porte <aklaporte@gmail.com>
2022-12-22 18:38:19 -0500

    Rename variables for clarity

--- day-6/python/solution.py
+++ day-6/python/solution.py
@@ -1,17 +1,15 @@
-lines = open('../inputs/test.txt', 'r').read().splitlines()
-signals = [line for line in lines]
+signals = open('../inputs/test.txt', 'r').read().splitlines()
 
 
-def find_marker_end(signals, size):
-    marker_size = size
+def find_marker_end(signals, marker_size):
     indices = []
     for signal in signals:
-        for i in range(0, len(signal)):
+        for i in range(len(signal)):
             if len(set(signal[i:i + marker_size])) == marker_size:
                 indices.append(i + marker_size)
                 break
     return indices
 
 
-print(*find_marker_end(signals, 4))
-print(*find_marker_end(signals, 14))
+print("Signals start at indices: {}".format(find_marker_end(signals, 4)))
+print("Signals start at indices: {}".format(find_marker_end(signals, 14)))
