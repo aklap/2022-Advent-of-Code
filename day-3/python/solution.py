4df2113a5fa9e2f1fed6d0550b5ae513febd6534
Alexis La Porte <aklaporte@gmail.com>
2022-12-22 16:27:02 -0500

    Fix Key Error when badge_set is empty

--- day-3/python/solution.py
+++ day-3/python/solution.py
@@ -17,14 +17,15 @@ def get_priority_sum(rucksacks):
     return sum
 
 
-def get_group_sum(rucksacks, group_size):
+def get_group_priority_sum(rucksacks, group_size):
     sum = 0
     for i in range(0, len(rucksacks), group_size):
         group_rucksacks = [set(rucksacks[i]) for i in range(i, i + group_size)]
         badge_set = group_rucksacks[0]
         for item_set in group_rucksacks[1:]:
             badge_set = badge_set.intersection(item_set)
-        sum += get_priority_score(badge_set.pop())
+        if len(badge_set) == 1:
+            sum += get_priority_score(badge_set.pop())
     return sum
 
 
