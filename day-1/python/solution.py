87432b9b6a05cf8cfc64696b043ea28f0e034fbd
Alexis La Porte <aklaporte@gmail.com>
2022-12-22 15:56:37 -0500

    Clean up formatting and refactor for clarity

--- day-1/python/solution.py
+++ day-1/python/solution.py
@@ -1,10 +1,7 @@
 with open('../inputs/input.txt', 'r') as f:
-    lines = f.read().strip().split("\n\n")
-    loads_per_elf = [load.split("\n") for load in lines]
-    calories_per_elf = [sum([int(cals) for cals in load]) for load in loads_per_elf]
-    calories_per_elf.sort()
-    highest_cals = calories_per_elf[-1]
-    highest_three_total_cals = sum(calories_per_elf[-3:])
-
-    print("The largest load was {:,} calories.".format(highest_cals))
-    print("The top 3 largest loads were: {:,}.".format(highest_three_total_cals))
+    loads = [load.split("\n") for load in f.read().strip().split("\n\n")]
+    calories_per_elf = [sum([int(cals) for cals in load]) for load in loads]
+    biggest_caloric_load = sorted(calories_per_elf)[-1]
+    top_three_caloric_loads = sum(sorted(calories_per_elf)[-3:])
+    print("The largest load was {:,} calories.".format(biggest_caloric_load))
+    print("The top 3 largest loads were: {:,}.".format(top_three_caloric_loads))
