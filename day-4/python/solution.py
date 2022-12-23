2114935dcebf93fdca4d7e6e87371b69097a8de4
Alexis La Porte <aklaporte@gmail.com>
2022-12-23 17:57:01 -0500

    Move helper functions into `get_count`

--- day-4/python/solution.py
+++ day-4/python/solution.py
@@ -1,26 +1,27 @@
-def is_subset(a, b):
-    return a.issubset(b) or b.issubset(a)
-
-
-def is_intersection(a, b):
-    return not a.isdisjoint(b)
-
-
 def get_count(pairs):
-    subset_count = 0
-    intersect_count = 0
+    counts = {
+        'subsets': 0,
+        'intersections': 0,
+    }
+
+    def is_subset(a, b):
+        return a.issubset(b) or b.issubset(a)
+
+    def is_intersection(a, b):
+        return not a.isdisjoint(b)
+
     for pair in pairs:
         [a_start, a_end], [b_start, b_end] = pair
         a = set(range(int(a_start), int(a_end) + 1))
         b = set(range(int(b_start), int(b_end) + 1))
-        subset_count += (1 if is_subset(a, b) else 0)
-        intersect_count += (1 if is_intersection(a, b) else 0)
-    return (subset_count, intersect_count)
+        counts['subsets'] += (1 if is_subset(a, b) else 0)
+        counts['intersections'] += (1 if is_intersection(a, b) else 0)
+    return counts
 
 
 with open('../inputs/input.txt', 'r') as f:
     sections = f.read().splitlines()
     pairs = [[pair.split('-') for pair in pair.split(',')] for pair in sections]
-    subset_count, intersect_count = get_count(pairs)
-    print("Number of sections with subsets: {}".format(subset_count))
-    print("Number of subsets with intersections: {}".format(intersect_count))
+    counts = get_count(pairs)
+    print("No. sections with subsets: {}".format(counts['subsets']))
+    print("No. intersecting sections: {}".format(counts['intersections']))
